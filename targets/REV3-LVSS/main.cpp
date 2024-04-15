8402db652530d3a301aa8afeca9ba996bb0e1971
eaton <ew9170@rit.edu>
2024-04-15 19:58:31 -0400

    Changed switch_schematic image and constructed the pins

--- targets/REV3-LVSS/main.cpp
+++ targets/REV3-LVSS/main.cpp
@@ -48,11 +48,43 @@ int main() {
     log::LOGGER.setUART(&uart);
     log::LOGGER.setLogLevel(log::Logger::LogLevel::INFO);
 
-    IO::GPIO& lvssPowerSwitchEnable0 = IO::getGPIO<IO::Pin::PA_10>(IO::GPIO::Direction::OUTPUT);
-    IO::GPIO& lvssPowerSwitchEnable1 = IO::getGPIO<IO::Pin::PA_9>(IO::GPIO::Direction::OUTPUT);
-    IO::GPIO& lvssPowerSwitchEnable2 = IO::getGPIO<IO::Pin::PA_8>(IO::GPIO::Direction::OUTPUT);
+    IO::GPIO& lvssPowerSwitch0Enable0 = IO::getGPIO<IO::Pin::PC_14>(IO::GPIO::Direction::OUTPUT); // hib
+    IO::GPIO& lvssPowerSwitch0Enable1 = IO::getGPIO<IO::Pin::PD_2>(IO::GPIO::Direction::OUTPUT); // battery
+    IO::GPIO& lvssPowerSwitch0Latch = IO::getGPIO<IO::Pin::PC_10>(IO::GPIO::Direction::OUTPUT); // latch
 
-    IO::GPIO* powerSwitches[3] = {&lvssPowerSwitchEnable0, &lvssPowerSwitchEnable1, &lvssPowerSwitchEnable2};
+    IO::GPIO& lvssPowerSwitch1Enable0 = IO::getGPIO<IO::Pin::PF_1>(IO::GPIO::Direction::OUTPUT); // hudl
+    IO::GPIO& lvssPowerSwitch1Enable1 = IO::getGPIO<IO::Pin::PA_0>(IO::GPIO::Direction::OUTPUT); // tms
+    IO::GPIO& lvssPowerSwitch1Latch = IO::getGPIO<IO::Pin::PC_3>(IO::GPIO::Direction::OUTPUT); // latch
+
+    IO::GPIO& lvssPowerSwitch2Enable0 = IO::getGPIO<IO::Pin::PA_1>(IO::GPIO::Direction::OUTPUT); // gub
+    IO::GPIO& lvssPowerSwitch2Enable1 = IO::getGPIO<IO::Pin::PC_8>(IO::GPIO::Direction::OUTPUT); // acc
+    IO::GPIO& lvssPowerSwitch2Latch = IO::getGPIO<IO::Pin::PB_14>(IO::GPIO::Direction::OUTPUT); // latch
+
+    IO::GPIO& diagEnable = IO::getGPIO<IO::Pin::PC_13>(IO::GPIO::Direction::OUTPUT); // diag enable
+    IO::GPIO& diagSelect1 = IO::getGPIO<IO::Pin::PC_15>(IO::GPIO::Direction::OUTPUT); // diag select 1
+    IO::GPIO& diagSelect2 = IO::getGPIO<IO::Pin::PF_0>(IO::GPIO::Direction::OUTPUT); // diag select 2
+
+    IO::ADC& lvssPowerSwitch0SenseOut = IO::getADC<IO::Pin::PC_0>();
+    IO::ADC& lvssPowerSwitch1SenseOut = IO::getADC<IO::Pin::PC_1>();
+    IO::ADC& lvssPowerSwitch2SenseOut = IO::getADC<IO::Pin::PC_2>();
+
+    LVSS::TPS2HB50BQ1 powerSwitch0 = LVSS::TPS2HB50BQ1(lvssPowerSwitch0Enable0, lvssPowerSwitch0Enable1,
+                                                       lvssPowerSwitch0Latch, diagEnable,
+                                                       diagSelect1, diagSelect2,
+                                                       lvssPowerSwitch0SenseOut);
+
+    LVSS::TPS2HB50BQ1 powerSwitch1 = LVSS::TPS2HB50BQ1(lvssPowerSwitch1Enable0, lvssPowerSwitch1Enable1,
+                                                         lvssPowerSwitch1Latch, diagEnable,
+                                                         diagSelect1, diagSelect2,
+                                                         lvssPowerSwitch1SenseOut);
+
+    LVSS::TPS2HB50BQ1 powerSwitch2 = LVSS::TPS2HB50BQ1(lvssPowerSwitch2Enable0, lvssPowerSwitch2Enable1,
+                                                            lvssPowerSwitch2Latch, diagEnable,
+                                                            diagSelect1, diagSelect2,
+                                                            lvssPowerSwitch2SenseOut);
+
+
+    LVSS::TPS2HB50BQ1* powerSwitches[3] = {&powerSwitch0, &powerSwitch1, &powerSwitch2};
 
     // initialize timer? probably don't need
     DEV::Timerf3xx timer(TIM2, 160);
@@ -96,7 +128,7 @@ int main() {
     // Initialize all the CANOpen dev.
     IO::initializeCANopenDriver(&canOpenQueue, &can, &timer, &canStackDriver, &nvmDriver, &timerDriver, &canDriver);
 
-    LVSS::LVSS lvss = LVSS::LVSS(reinterpret_cast<LVSS::TPS2HB50BQ1**>(powerSwitches));
+    LVSS::LVSS lvss = LVSS::LVSS(powerSwitches);
 
     // Initialize the CANOpen node we are using.
     IO::initializeCANopenNode(&canNode, &lvss, &canStackDriver, sdoBuffer, appTmrMem);
