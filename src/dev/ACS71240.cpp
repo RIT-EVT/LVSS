a6e85b7d5c2ed3edc17494b8c49b12a3cbe41d54
Jesuschees <dtrapp@hcrhs.org>
2025-09-24 13:34:56 -0400

    Just updating comment to be a little more helpful. Still don't know but that's my best guess.

--- src/dev/ACS71240.cpp
+++ src/dev/ACS71240.cpp
@@ -9,7 +9,7 @@ int32_t ACS71240::readCurrent() {
     int32_t adcCounts = ADC.readRaw();
     constexpr int32_t voltIn = 3300; //millivolts
     constexpr int32_t sensitivity = 44; //millivolts / amp
-    constexpr int32_t avgAdcCount = 1970; //lowk this number was here when i got here, will check
+    constexpr int32_t avgAdcCount = 1970; //Is this number like the background noise?
 
     //(((adcCounts - average adc counts) * inputVoltage) / (sensitivity * 2^12)/100)
     int32_t current = (((adcCounts - avgAdcCount) * voltIn) / (sensitivity << 12)/100);
